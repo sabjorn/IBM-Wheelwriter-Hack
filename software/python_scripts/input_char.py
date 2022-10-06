37c9cf4a76ac5b39a4e6465551d937f1123d12b8
sabjorn <info@sabjorn.net>
2022-10-06 14:09:23 -0700

    add simple script for single character direct input from python to typewriter

---
+++ software/python_scripts/input_char.py
@@ -0,0 +1,21 @@
+import serial
+import sys, tty, termios
+def get_ch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+ser = serial.Serial("/dev/cu.usbserial-1420", 115200, timeout=0.1)
+# single character at a time
+stringHeader = chr(0x00) + chr(0x00) + chr(1 >> 8)
+while True:
+    try:
+        ch = get_ch()
+        ser.write(stringHeader.encode() + ch.encode())
+    except:
+        break
